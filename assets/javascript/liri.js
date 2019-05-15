8f1b9e7a9004a52d5b519c8d39ca323fc6449378
Devin Powell <dpowel11@kent.edu>
2019-05-15 11:42:16 -0600

    Finished the OMDB Functionality

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -19,6 +19,100 @@ var spotify = new Spotify({
   secret: '3560fa1fe4c64a6c8f62ebeececdfdf8'
 });
 
+
+// OMDB Function if the choice is to search a movie
+function searchOMDB(queryURL) { 
+  // Take in the URL and console log Movie Info
+  request(queryURL,function(err, response, body) {
+    console.log('response:', JSON.parse(body));
+    if (!err && response.statusCode === 200) {
+    // Log movie info
+    console.log(`=======================================`);
+    console.log('Title:', JSON.parse(body).Title);
+    console.log('IMDB Rating:', JSON.parse(body).imdbRating);
+    console.log('Rotten Tomatoes:', JSON.parse(body).Ratings[1].Value);
+    console.log('Country:', JSON.parse(body).Country);
+    console.log('Language:', JSON.parse(body).Language);
+    console.log('Plot:', JSON.parse(body).Plot);
+    console.log('Actors:', JSON.parse(body).Actors);
+    console.log(`=======================================`);
+    }
+  });
+}
+
+
+// Inquirer Flow
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'userInput',
+    message: 'Hello. What can I do for you today?',
+    choices: ['Find Movie','Find Song','Obedience is my purpose...']
+  }
+]).then(function(command) {
+  // console.log('User Command', command); // Comes back as an object called userInput
+
+  // If User wants to find a movie
+  if (command.userInput === 'Find Movie') {
+    console.log('User Command:', command);
+
+    // Ask which movie to search for
+    inquirer.prompt([
+      {
+        type: 'input',
+        name:'movieTitle',
+        message: 'Which movie would you like information on?',
+      }
+    ]).then(function(movie) {
+      // console.log('Movie Title:', movie); // Comes back as object called movieTitle
+
+      // If movie field is empty
+      if (movie.movieTitle === '') {
+        var queryURL='http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy'
+        // If the user entered a movie name
+      } else {
+        // Build the queryURL
+        var movieTitle = movie.movieTitle.replace(/ /g,'+');
+        var queryURL = 'http://www.omdbapi.com/?t=' + movieTitle + '&y=&plot=short&apikey=trilogy';
+        // console.log('queryURL:', queryURL); // Works correctly now
+      }
+      searchOMDB(queryURL);
+    })
+  }
+
+
+
+
+
+
+
+  // If User wants to find a song
+  // If User wants Liri to do what she's told
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Ex Input
 // node liri.js spotify-this-song '<song name here>'
 
@@ -39,29 +133,6 @@ var spotify = new Spotify({
 
 
 
-// OMDB Function if the choice is to search a movie
-function searchOMDB(url) { 
-  // Take in the URL and console log Movie Info
-  request(url,function(err, response) {
-    if (!error && response.statusCode === 200) {
-    // Log movie info
-    console.log(`=======================================`);
-    console.log(`Title: ${response.data.Title}`);
-    console.log(`IMDB Rating: ${response.data.imdbRating}`);
-    console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-    console.log(`Country: ${response.data.Country}`);
-    console.log(`Language: ${response.data.Language}`);
-    console.log(`Plot: ${response.data.Plot}`);
-    console.log(`Actors: ${response.data.Actors}`);
-    console.log(`=======================================`);
-    }
-  })
-}
 
-
-
-
-// Axios Request
-var axios = require('axios');
 
 
